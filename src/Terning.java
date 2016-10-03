b1698470c93dacc6a702a7903911fce58eb2cc93
khurramsaeedmalik1994 <s165162@student.dtu.dk>
2016-10-03 13:24:06 +0200

    ændringer test

---
+++ src/Terning.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+/**
+ * Created by Khurrrmiiii on 30/09/2016.
+ */
+public class Terning {
+    spiller sp = new spiller();
+
+
+    //Class-diagram: Terning
+    Random rand = new Random();
+    int t1 = rand.nextInt(5) + 1;
+    int t2 = rand.nextInt(5) + 1;
+    int sum = t1 + t2;
+    //Definere 2 spillere'
+    void tjekker(int player1, int player2,int validate){
+        player1=1;
+        player2=2;
+        validate=0;
+        if(validate==player1){
+            sum=t1+t2
+        }
+        else if(validate==player2){
+            int sum1=t1+t2;
+        }
+
+    }
+    }
+
