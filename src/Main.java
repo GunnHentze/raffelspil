cc54fe708210b0eba3629577d56a2c51211914fc
khurramsaeedmalik1994 <s165162@student.dtu.dk>
2016-10-03 14:50:19 +0200

    Ny main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        spiller sp = new spiller();
-        System.out.println("Welcome: "+sp.toString());
+	public static void main(String[] args) {
+		spiller sp = new spiller();
+		Terning t = new Terning();
+		Point p = new Point();
 
-        Terning cup = new Terning();
-        System.out.println("Slag: " + cup.t1 + ", " + cup.t2);
-        System.out.println("Summen af de to slag: "+ cup.sum);
+		System.out.println("Welcome: " + sp.toString());
 
-        }
-    }
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			System.out.println("Player press 1 to roll");
+
+			int a = sc.nextInt();
+
+			if (a == 1) {
+				p.validate = 1;
+				System.out.println("Slag: " + t.t1 + ", " + t.t2);
+			} else {
+				System.out.println("Error");
+			}
+
+			System.out.println("Player 2 press 2 to roll");
+			int b = sc.nextInt();
+
+			if (b == 2) {
+				p.validate = 2;
+				System.out.println("Slag: " + t.t3 + ", " + t.t4);
+			} else {
+				System.out.println("Error");
+			}
+
+		}
+	}
+}
