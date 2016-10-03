561f3725a6f779f65ebf5a4b2a48ac171726f6fc
khurramsaeedmalik1994 <s165162@student.dtu.dk>
2016-10-03 12:55:21 +0200

    Update 1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        spiller sp1 = new spiller();
-        System.out.println("Welcome: "+sp1.toString());
+        spiller sp = new spiller();
+        System.out.println("Welcome: "+sp.toString());
 
-
-        cup cup = new cup();
+        Terning cup = new Terning();
         System.out.println("Slag: " + cup.t1 + ", " + cup.t2);
         System.out.println("Summen af de to slag: "+ cup.sum);
 
-
+        }
     }
-}
--- src/terning.java
+++ src/terning.java
@@ -1,5 +1,19 @@
+import java.util.Random;
+
 /**
  * Created by Khurrrmiiii on 30/09/2016.
  */
-public class terning {
-}
+public class Terning {
+    spiller sp = new spiller();
+    //Definere 2 spillere
+    int player1 = 1;
+    int player2 = 2;
+    int validate = 0;
+
+    //Class-diagram: Terning
+        Random rand = new Random();
+        int t1 = rand.nextInt(5) + 1;
+        int t2 = rand.nextInt(5) + 1;
+        int sum = t1 + t2;
+    }
+
