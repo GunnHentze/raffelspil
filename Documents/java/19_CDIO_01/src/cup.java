1868d4f2e95675c438e857eee239cb06ecd05d77
khurramsaeedmalik1994 <s165162@student.dtu.dk>
2016-10-03 13:20:31 +0200

    Changed

---
+++ Documents/java/19_CDIO_01/src/cup.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+/**
+ * Created by Khurrrmiiii on 30/09/2016.
+ */
+public class cup {
+
+    //Sum af terninger
+        Random rand = new Random();
+        int t1 = rand.nextInt(5) + 1;
+        int t2 = rand.nextInt(5) + 1;
+        int sum = t1 + t2;
+    }
+
