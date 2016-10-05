ec285da61636a15e1fe7574c523d508367659f0f
Swagam <Swagam@agams-mbp.hhk.dk>
2016-10-05 11:01:47 +0200

    Change in line 11

--- Spiller.java
+++ Spiller.java
@@ -5,10 +5,11 @@ public class Spiller {
     public String toString(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Indstast Spiller 1: ");
-        String player1 = sc.nextLine();
+        String spiller1 = sc.nextLine();
         System.out.println("Indstast Spiller 2: ");
-        String player2 = sc.nextLine();
-        return player1+" and "+player2;
+        String spiller2 = sc.nextLine();
+        return spiller1+" and "+spiller2;
     }
 }
-}
+
+//hej
