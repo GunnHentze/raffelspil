e5cd352d3fa8c90acd841a5587f1e0b361b4cbd8
khurramsaeedmalik1994 <s165162@student.dtu.dk>
2016-10-05 18:46:31 +0200

    -Khurram redigeret -delete

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,183 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Random;
 import java.util.Scanner;
 
+/**
+ *
+ * @author Injam DCF
+ */
 public class Main {
 
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
 	public static void main(String[] args) {
+		int player1 = 0;
+		int player2 = 0;
 
-		Game p = new Game();
+		System.out.println("Velkommen til Terning Splillet");
 
-		//System.out.println("Welcome: " + sp.toString());
-
-		Scanner sc = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
+		
+		Roll r = new Roll();
 
 		while (true) {
-			System.out.println("Player press 1 to roll");
-
-			int a = sc.nextInt();
-
-			if (a == 1) {
-				p.validate = 1;
-				System.out.println("Slag: " + p.roll() + ", " + p.roll());
-				System.out.println("Player 1 total is: "+ p.PointSum());
-				
-			} 
 			
-			else {
-				System.out.println("Error");//
+			int x = r.terning1();
+			
+			int y = r.terning2();
+
+			System.out.println("Player 1, Tryk 1 for at kaste");
+
+			if (input.nextInt() == 1) {
+
+				Random rand = new Random();
+
+				System.out.println("Du slog " + x + " og " + y);
+
+				player1 = player1 + y + x;
+
+				if (x == 1 && y == 1) {
+					player1 = 0;
+					System.out.println("Du slog to 1'ere. Du mister dine point!");
+				}
+
+				if (x == y && x > 1) {
+
+					// Når man rammer 40 point skal man slå to af de samme tal
+					if (player1 >= 40) {
+
+						if (x == y) {
+							System.out.println("Du vandt");
+							break;
+						}
+					}
+
+					if (x == 6 && y == 6) {
+						System.out.println("Du slog 2 6'ere, hvis du slår 6'ere igen");
+
+					}
+
+					System.out.println("Du får en ekstra tur!");
+					x = rand.nextInt(6) + 1;
+					y = rand.nextInt(6) + 1;
+					System.out.println("Du slog i din ekstra tur " + x + " og " + y);
+					player1 = player1 + y + x;
+
+					if (x == y) {
+
+						System.out.println("Du får en ekstra tur!");
+						x = rand.nextInt(6) + 1;
+						y = rand.nextInt(6) + 1;
+						System.out.println("Du slog i din ekstra tur " + x + " og " + y);
+						player1 = player1 + y + x;
+
+						if (x == y) {
+							System.out.println("Du får en ekstra tur!");
+							x = rand.nextInt(6) + 1;
+							y = rand.nextInt(6) + 1;
+							System.out.println("Du slog i din ekstra tur " + x + " og " + y);
+							player1 = player1 + y + x;
+
+						}
+
+					}
+
+				}
+
+				System.out.println("Player 1 total er " + player1);
+
+				// Når man rammer 40 point skal man slå to af de samme tal
+				if (player1 >= 40) {
+
+					if (x == y) {
+						System.out.println("Du vandt");
+						break;
+					}
+				}
 			}
-
-			System.out.println("Player 2 press 2 to roll");
-			int b = sc.nextInt();
-
-			if (b == 2) {
-				p.validate = 2;
-				System.out.println("Slag: " + p.roll() + ", " + p.roll());
-				System.out.println("Player 2 total is: " + p.PointSum1() );
-				System.out.println(p.P2);
-			} 
 			
-			else {
-				System.out.println("Error");
+            int x1 = r.terning1();
+			
+			int y1 = r.terning2();
+
+			System.out.println("Player 2 tryk 2 for at kaste");
+			if (input.nextInt() == 2) {
+
+				Random rand = new Random();
+
+				System.out.println("Du slog " + x1 + " og " + y);
+
+				player2 = player2 + x1 + y;
+
+				// To 1'ere mister alle point.
+				if (x1 == 1 && y == 1) {
+					player2 = 0;
+					System.out.println("Du slog to 1'ere. Du mister dine point!");
+				}
+
+				if (x1 == y && x1 > 1) {
+
+					// Når man rammer 40 point skal man slå to af de samme tal
+					if (player2 >= 40) {
+
+						if (x1 == y) {
+							System.out.println("Du vandt");
+							break;
+						}
+					}
+					if (x1 == 6 && y == 6) {
+						System.out.println("Du slog 2 6'ere, hvis du slår 6'ere igen");
+
+					}
+
+					System.out.println("Du får en ekstra tur!");
+					x1 = rand.nextInt(6) + 1;
+					y = rand.nextInt(6) + 1;
+					System.out.println("Du slog i din ekstra tur " + x1 + " og " + y);
+					player2 = player2 + y + x1;
+
+					if (x1 == y) {
+
+						System.out.println("Du får en ekstra tur!");
+						x1 = rand.nextInt(6) + 1;
+						y = rand.nextInt(6) + 1;
+						System.out.println("Du slog i din ekstra tur " + x1 + " og " + y);
+						player2 = player2 + y + x1;
+
+						if (x1 == y) {
+							System.out.println("Du får en ekstra tur!");
+							x1 = rand.nextInt(6) + 1;
+							y = rand.nextInt(6) + 1;
+							System.out.println("Du slog i din ekstra tur " + x1 + " og " + y);
+							player2 = player2 + y + x1;
+
+						}
+
+					}
+
+				}
+
+				System.out.println("Player 2 total er " + player2);
+
+				// Når man rammer 40 point skal man slå to af de samme tal
+				if (player2 >= 40) {
+
+					if (x1 == y) {
+						System.out.println("Du vandt");
+						break;
+					}
+				}
+
 			}
 
 		}
+
 	}
 }
