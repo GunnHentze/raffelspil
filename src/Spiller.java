500f4d2a355f7efb1570b92d8eab1ea49b9671e0
khurramsaeedmalik1994 <s165162@student.dtu.dk>
2016-10-05 18:48:10 +0200

    Added files Roll.java & Spiller.java

---
+++ src/Spiller.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Spiller {
+
+    public String toString() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Indstast Spiller 1: ");
+        String spiller1 = sc.nextLine();
+        System.out.println("Indstast Spiller 2: ");
+        String spiller2 = sc.nextLine();
+		return spiller1 +" and "+ spiller2;
+		
+    }
+}
